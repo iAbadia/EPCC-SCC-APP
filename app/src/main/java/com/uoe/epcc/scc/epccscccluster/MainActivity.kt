9a7808484f03931dd4ea7d3592115574640bdc62
Inaki Abadia <i.abadiaosta@gmail.com>
2018-01-18 18:09:34 +0000

    Initial commit Adds initial Activity and resources Adds gradle files

---
+++ app/src/main/java/com/uoe/epcc/scc/epccscccluster/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.uoe.epcc.scc.epccscccluster
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Enter immersive mode
+        hideSystemUI()
+    }
+    /**
+     * Enter Immersive mode. Hides status and navigation bars, both
+     * will be still reachable by swiping up/down on the edge of the
+     * screen
+     * */
+    fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+}
